fc6c934496c562afdbc8df60342169f6cebd8adb
WA11Awa1la <34579634+WA11Awa1la@users.noreply.github.com>
2018-04-11 15:23:58 -0400

    Update VisualStudio.bat

--- VisualStudio.bat
+++ VisualStudio.bat
@@ -1,1 +1,1 @@
-vs_buildtools.exe --norestart --quiet --wait --add Microsoft.VisualStudio.Workload.MSBuildTools --add Microsoft.VisualStudio.Workload.WebBuildTools --add Microsoft.VisualStudio.Workload.VCTools
+vs_buildtools.exe --wait --norestart --quiet --add Microsoft.VisualStudio.Workload.MSBuildTools --add Microsoft.VisualStudio.Workload.WebBuildTools --add Microsoft.VisualStudio.Workload.VCTools
